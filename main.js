55b9e0f9cb3015e41be0f2c3899ab2b83c4b3ab1
liuyongtao <lyongtao123@126.com>
2021-02-22 15:06:24 +0800

    修改sku引入方式

--- main.js
+++ main.js
@@ -11,12 +11,10 @@ Vue.component('DialogBox', DialogBox);
 //mescroll
 import MescrollBody from "@/components/mescroll-uni/mescroll-body.vue"
 import MescrollUni from "@/components/mescroll-uni/mescroll-uni.vue"
-import VkDataGoodsSkuPopup from '@/components/SkuPopup/vk-data-goods-sku-popup/vk-data-goods-sku-popup.vue';
-import VkDataInputNumberBox from '@/components/SkuPopup/vk-data-input-number-box/vk-data-input-number-box.vue';
+
 Vue.component('mescroll-body', MescrollBody)
 Vue.component('mescroll-uni', MescrollUni)
-Vue.component('vk-data-goods-sku-popup', VkDataGoodsSkuPopup)
-Vue.component('vk-data-input-number-box', VkDataInputNumberBox)
+
 
 App.mpType = 'app'
 
